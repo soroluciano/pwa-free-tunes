9cbf48918e08262058d5d972faf46c0781b026b1
Luciano Soro <soro.luc@gmail.com>
2020-11-09 13:34:34 +0100

    modicando llamada a webpushr

--- webpushr-sw.js
+++ webpushr-sw.js
@@ -1,1 +1,5 @@
-importScripts('https://cdn.webpushr.com/sw-server.min.js');
+fetchInject([
+    'https://cdn.webpushr.com/sw-server.min.js'
+  ]).then(() => {
+    console.log(`Finish in less than ${moment().endOf('year').fromNow(true)}`)
+  })
